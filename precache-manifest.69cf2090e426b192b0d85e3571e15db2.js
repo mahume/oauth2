4202d4939001db5696d51858c764feb79fd787a4
mahume <michael.a.hume@gmail.com>
2019-10-06 19:42:57 -0600

    Updates

---
+++ precache-manifest.69cf2090e426b192b0d85e3571e15db2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14a85f5a3b129119f94f6a8ec357b06",
+    "url": "/oauth2/index.html"
+  },
+  {
+    "revision": "8685a061be15f776c1e7",
+    "url": "/oauth2/static/js/2.17cb4368.chunk.js"
+  },
+  {
+    "revision": "c627b7f3ce0029ea1c21",
+    "url": "/oauth2/static/js/main.0ec00605.chunk.js"
+  },
+  {
+    "revision": "8d3138a6a42ac02c74dc",
+    "url": "/oauth2/static/js/runtime-main.2e7e91f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%20//mahume.github.io/oauth2/precache-manifest.5e28bd5e1ae1302e4f49973ad23021d1.js"
+  "/oauth2/precache-manifest.69cf2090e426b192b0d85e3571e15db2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%20//mahume.github.io/oauth2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oauth2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
